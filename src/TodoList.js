484094a9bb72a3185f8658bb22f5871bdecc264c
brownguo <yexuan.guo@codedance.cn>
2020-02-23 18:55:43 +0800

    function to class

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,35 @@
-import React,{Fragment}from 'react';
-
+import React,{ Fragment, Component }from 'react';
 // Fragment 占位符，影藏最外层的dom标签
+/*
 function TodoList() {
     return (
         <Fragment>
-            <input/> <button>Go!!!</button>
+            <div>
+                <input/> <button>Go!!!</button>
+            </div>
+
             <ul>
                 <li>First!</li>
             </ul>
         </Fragment>
     )
+}*/
+
+class TodoList extends Component{
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <input/> <button>Go!!!</button>
+                </div>
+
+                <ul>
+                    <li>Go!</li>
+                    <li>Go!!</li>
+                </ul>
+            </Fragment>
+        )
+    }
 }
 
 export default TodoList;
