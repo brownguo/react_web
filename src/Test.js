ed6bff454350d530dafa757fa26789dc07b84892
brownguo <yexuan.guo@codedance.cn>
2020-02-25 20:15:10 +0800

    Test~

--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,50 @@
-import React,{ Component } from 'react'
+import React,{ Component,Fragment} from 'react'
 
 class Test extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            inputValue:'',
+            list:['北京','上海','呼和浩特']
+        }
+    }
     render() {
-        return <div>{this.props.content}</div>
+        return (
+            <Fragment>
+                <input value={this.state.inputValue} onChange={this.handleInput.bind(this)} />
+                <button onClick={this.handleButtonClick.bind(this)}>提交</button>
+                <ul>
+                    {
+                        this.state.list.map((item,index) =>{
+                            return(
+                                <li key={index} onClick={this.handleDelete.bind(this,index)}>{item}</li>
+                            )
+                        })
+                    }
+                </ul>
+            </Fragment>
+        )
+    }
+
+    handleInput(e){
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+
+    handleDelete(index){
+        this.setState((prevState) =>{
+            const list = [...prevState.list];
+            list.splice(index,1)
+            return {list}
+        })
+    }
+
+    handleButtonClick(e){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:'',
+        })
     }
 }
 export default Test;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import TodoList from './TodoList';
 import AntdLayoutTodolist from './AntdLayoutTodolist';
-
+import Test from './Test'
 //ReactDOM.render(<TodoList />, document.getElementById('root'));
 
 //Antd
 // import AntdTodoList from './AntdTodoList'
 // ReactDOM.render(<AntdTodoList />, document.getElementById('root-antd'));
 
-ReactDOM.render(<AntdLayoutTodolist />, document.getElementById('root'));
+ReactDOM.render(<Test />, document.getElementById('root'));
 
