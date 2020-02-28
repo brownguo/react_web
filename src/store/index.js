7383a4103b45d54735cf51a07f8fef3be23bdda3
brownguo <yexuan.guo@codedance.cn>
2020-02-28 22:34:18 +0800

    redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
-
 //Store做数据分发，创建store的时候先要把reducer传过去。
-import { createStore } from 'redux'
+import { createStore,applyMiddleware } from 'redux'
 import reducer from './reducer';
 
 //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
